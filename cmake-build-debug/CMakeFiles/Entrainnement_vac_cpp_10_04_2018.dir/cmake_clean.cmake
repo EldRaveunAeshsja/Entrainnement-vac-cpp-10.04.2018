4e1f7540ee0d48831339eeed785cd58c00e8fa8d
EldRaveunAeshsja <arnaud.gonin@live.fr>
2018-04-10 15:51:56 +0200

    init du projet

---
+++ cmake-build-debug/CMakeFiles/Entrainnement_vac_cpp_10_04_2018.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Entrainnement_vac_cpp_10_04_2018.dir/main.cpp.obj"
+  "Entrainnement_vac_cpp_10_04_2018.pdb"
+  "Entrainnement_vac_cpp_10_04_2018.exe"
+  "Entrainnement_vac_cpp_10_04_2018.exe.manifest"
+  "libEntrainnement_vac_cpp_10_04_2018.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Entrainnement_vac_cpp_10_04_2018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Entrainnement_vac_cpp_10_04_2018.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Entrainnement_vac_cpp_10_04_2018.
+# This may be replaced when dependencies are built.
