a2060e1154710156af3d17e2561c1055b28f3a64
EldRaveunAeshsja <arnaud.gonin@live.fr>
2018-04-13 12:48:55 +0200

    melange de getline et cin, utiliation du cin.ignore

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int chiffre(26);
-    int& monChiffreEnRef(chiffre);
+    cout << "Combien vaut pi ?" << endl;
+    double piUtilisateur(-1.); //On crée une case mémoire pour stocker unnombre réel
+    cin >> piUtilisateur; //Et on remplit cette case avec ce qu'écritl'utilisateur
 
-    cout << chiffre << endl;
+    cin.ignore();
 
-    cout << monChiffreEnRef << endl;
+    cout << "Quel est votre nom ?" << endl;
+    string nomUtilisateur("Sans nom"); //On crée une case mémoire pour contenir une chaine de caractères
+    getline(cin, nomUtilisateur); //On remplit cette case avec toute la ligne que l'utilisateur a écrit
 
-   return 0;
+    cout << "Vous vous appelez " << nomUtilisateur << " et vous pensez que pi vaut " << piUtilisateur << "." << endl;
+
+    return 0;
 }
